b672d00ebe527b46de6ef12f2f64edcacbd9917b
Lukas Seo <lukas.seo@mail.ru>
2019-05-13 16:16:44 -0400

    Exponential number generator for emacs lisp

---
+++ exponential-generator.el
@@ -0,0 +1,29 @@
+(require 'generator)
+(setq lexical-binding t)
+
+(iter-defun exp-gen (pow)
+  (let ((i 0))
+    (while
+      (setq i (1+ i))
+      (iter-yield (expt i pow)))))
+
+(ffloor 3.1)
+
+(iter-defun flt-gen ()
+  (let ((i 'nil)
+	(g (exp-gen 2))
+	(f (lambda (n)
+	     (if (= (ffloor (log n 3)) (log n 3))
+		 'nil
+	       't))))
+    (while
+	(setq i (iter-next g))
+      (when (funcall f i)
+	(iter-yield i)))))
+
+(let ((g (flt-gen))
+      (o 'nil))
+  (dotimes (i 29)
+    (setq o (iter-next g))
+    (when (> i 18)
+      (print o))))
