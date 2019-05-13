e01899ffdefc000ea7a8f434f6bbe160b750aae1
Lukas Seo <lukas.seo@mail.ru>
2019-05-13 18:14:22 -0400

    I suck at math. New test logic is sound

--- exponential-generator.el
+++ exponential-generator.el
@@ -2,26 +2,27 @@
 (setq lexical-binding t)
 
 (iter-defun exp-gen (pow)
-  (let ((i 0))
+  (let ((i -1))
     (while
       (setq i (1+ i))
       (iter-yield (expt i pow)))))
 
 (iter-defun flt-gen ()
-  (let ((i 'nil)
-	(g (exp-gen 2))
-	(f (lambda (n)
-	     (if (= (ffloor (log n 3)) (log n 3))
-		 'nil
-	       't))))
+  (let* ((g (exp-gen 2))
+	 (f (exp-gen 3))
+	 (i (iter-next g))
+	 (j (iter-next f)))
     (while
-	(setq i (iter-next g))
-      (when (funcall f i)
+      (setq i (iter-next g))
+      (while (> i j)
+	(setq j (iter-next f)))
+      (unless (= i j)
 	(iter-yield i)))))
+	
 
 (let ((g (flt-gen))
       (o 'nil))
   (dotimes (i 29)
     (setq o (iter-next g))
-    (when (> i 18)
+    (when (>= i 20)
       (print o))))
